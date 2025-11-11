41e06870be3fa3e7657ca5cf63bfecd6cf2235f9
George Park <georgepark.dev@outlook.com>
2025-11-11 14:31:23 +0700

    feat: implement gallery component with dynamic image loading and structured data

---
+++ src/utils/gallery.ts
@@ -0,0 +1,477 @@
+const GalleryPaths = {
+  DrinkOff: [
+    {
+      src: '~/assets/images/gallery/DrinkOff/IMG-20200324-WA0000.webp',
+    },
+    {
+      src: '~/assets/images/gallery/DrinkOff/IMG-20200331-WA0006.webp',
+    },
+    {
+      src: '~/assets/images/gallery/DrinkOff/IMG-20200331-WA0008.webp',
+    },
+  ],
+  EU: [
+    {
+      src: '~/assets/images/gallery/EU/IMG_20190906_125136.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG_20190906_162258.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG_20190906_163244.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG_20190906_164330.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG_20201125_184403.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG_20201125_184418.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG_20201125_184446.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG_20201125_184510.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG-20190920-WA0001.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG-20190920-WA0002.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG-20190920-WA0003.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG-20190920-WA0004.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG-20190920-WA0005.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG-20190920-WA0006.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG-20191122-WA0007.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG-20191122-WA0008.webp',
+    },
+    {
+      src: '~/assets/images/gallery/EU/IMG-20191122-WA0009.webp',
+    },
+  ],
+  HoneyStics: [
+    {
+      src: '~/assets/images/gallery/HoneyStics/IMG_20210325_164633_885.webp',
+    },
+    {
+      src: '~/assets/images/gallery/HoneyStics/Screenshot_20210319-094310_Drive.webp',
+    },
+    {
+      src: '~/assets/images/gallery/HoneyStics/Screenshot_20210325-164717_Gallery.webp',
+    },
+  ],
+  PharmaChina: [
+    {
+      src: '~/assets/images/gallery/PharmaChina/FB_IMG_1606318785506.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/FB_IMG_1606318794555.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190513-WA0001.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190513-WA0003.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190513-WA0008.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190513-WA0011.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190513-WA0012.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190513-WA0013.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190513-WA0018.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190513-WA0026.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190513-WA0043.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190513-WA0044.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190513-WA0045.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190513-WA0046.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190514-WA0000.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190514-WA0001.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190514-WA0002.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190514-WA0010.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190514-WA0011.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190514-WA0012.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190514-WA0013.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190515-WA0010.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190517-WA0013.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190517-WA0016.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190517-WA0030.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190517-WA0035.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190517-WA0037.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190517-WA0038.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190517-WA0042.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190517-WA0044.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190517-WA0061.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190517-WA0066.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190518-WA0000.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190518-WA0003.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190518-WA0004.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190518-WA0005.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190518-WA0007.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190518-WA0010.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190518-WA0013.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190518-WA0014.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190518-WA0015.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190518-WA0021.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190518-WA0022.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190518-WA0025.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190817-WA0015.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190817-WA0020.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190817-WA0024.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190817-WA0028.webp',
+    },
+    {
+      src: '~/assets/images/gallery/PharmaChina/IMG-20190818-WA0008.webp',
+    },
+  ],
+  Production: [
+    {
+      src: '~/assets/images/gallery/Production/IMG-20200328-WA0002.webp',
+    },
+    {
+      src: '~/assets/images/gallery/Production/IMG-20200328-WA0003.webp',
+    },
+    {
+      src: '~/assets/images/gallery/Production/IMG-20200328-WA0005.webp',
+    },
+    {
+      src: '~/assets/images/gallery/Production/IMG-20200328-WA0008.webp',
+    },
+    {
+      src: '~/assets/images/gallery/Production/IMG-20200328-WA0012.webp',
+    },
+    {
+      src: '~/assets/images/gallery/Production/IMG-20200328-WA0013.webp',
+    },
+  ],
+  SticsChina: [
+    {
+      src: '~/assets/images/gallery/SticsChina/IMG-20190505-WA0000.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/IMG-20190505-WA0001.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/IMG-20190505-WA0002.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/IMG-20190505-WA0003.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/IMG-20190505-WA0004.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/IMG-20190505-WA0005.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/IMG-20190505-WA0006.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/IMG-20190505-WA0007.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/IMG-20190505-WA0008.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/IMG-20190510-WA0000.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/IMG-20190510-WA0001.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/IMG-20190510-WA0002.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/mmexport1590258570148.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/mmexport1597584384362.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/mmexport1609740783671.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/Screenshot_2020-08-16-17-18-46-024_com.taobao.taobao.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/Screenshot_2020-08-16-17-19-08-041_com.taobao.taobao.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/Screenshot_2020-08-16-17-19-27-938_com.taobao.taobao.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/Screenshot_2020-08-16-17-19-45-745_com.taobao.taobao.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/Screenshot_2020-08-16-17-19-59-219_com.taobao.taobao.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/Screenshot_2020-08-16-17-20-14-940_com.taobao.taobao.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/Screenshot_2020-08-16-17-20-31-539_com.taobao.taobao.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/Screenshot_2020-08-16-17-21-02-480_com.taobao.taobao.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/Screenshot_2020-08-16-17-21-26-911_com.taobao.taobao.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/Screenshot_2020-08-16-17-21-49-653_com.taobao.taobao.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/Screenshot_2020-08-16-17-22-07-816_com.taobao.taobao.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/Screenshot_2020-08-16-17-22-26-766_com.taobao.taobao.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/Screenshot_2020-08-16-17-22-43-095_com.taobao.taobao.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsChina/Screenshot_2020-08-16-17-23-00-359_com.taobao.taobao.webp',
+    },
+  ],
+  SticsUSA: [
+    {
+      src: '~/assets/images/gallery/SticsUSA/IMG-20200628-WA0002.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsUSA/IMG-20200628-WA0003.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsUSA/IMG-20200712-WA0001.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsUSA/IMG-20200712-WA0002.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsUSA/IMG-20200718-WA0001.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsUSA/Screenshot_2020-06-23-14-31-08-983_com.whatsapp.webp',
+    },
+    {
+      src: '~/assets/images/gallery/SticsUSA/Screenshot_2020-06-23-14-31-33-279_com.whatsapp.webp',
+    },
+  ],
+  TeaPromo: [
+    {
+      src: '~/assets/images/gallery/TeaPromo/400017000412_302354.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG-20191217-WA0001.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG-20191217-WA0003.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG-20200316-WA0003.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG-20210907-WA0000.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG-20210907-WA0001.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG-20210907-WA0002.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG-20210907-WA0003.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG_20200120_201030.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG_20200120_202006.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG_20200120_202054.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG_20200120_202058.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG_20200122_162714.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG_20200122_162731.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG_20200122_164905.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG_20200122_164910.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaPromo/IMG_20200122_165033.webp',
+    },
+  ],
+  TeaStics: [
+    {
+      src: '~/assets/images/gallery/TeaStics/400017000412_302354.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaStics/IMG-20190325-WA0002.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaStics/IMG-20190325-WA0003.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaStics/IMG-20190325-WA0004.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaStics/IMG-20190416-WA0005.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaStics/IMG-20190416-WA0006.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaStics/IMG-20200715-WA0001.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaStics/IMG-20200723-WA0003.webp',
+    },
+    {
+      src: '~/assets/images/gallery/TeaStics/IMG-20210906-WA0001.webp',
+    },
+  ],
+}
+
+export const GalleryData = [
+  {
+    title: 'Китай',
+    items: [
+      ...GalleryPaths.PharmaChina,
+      ...GalleryPaths.SticsChina,
+    ]
+  },
+  {
+    title: 'Европейский союз',
+    items: [
+      ...GalleryPaths.EU,
+    ]
+  },
+  {
+    title: 'Имбирно-ягодный чай "Живые витамины"',
+    items: [
+      ...GalleryPaths.TeaPromo,
+      ...GalleryPaths.TeaStics,
+    ]
+  },
+  {
+    title: 'США и Канада',
+    items: [
+      ...GalleryPaths.SticsUSA,
+    ]
+  },
+  {
+    title: 'Дринкофф',
+    items: [
+      ...GalleryPaths.Production,
+      ...GalleryPaths.DrinkOff,
+      ...GalleryPaths.HoneyStics,
+    ]
+  },
+]
